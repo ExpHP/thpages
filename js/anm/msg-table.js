dc2b8b20331e1b4253389aa7ff88353fb50d940e
Michael Lamparski <diagonaldevice@gmail.com>
2021-04-28 20:37:02 -0400

    more MSG updates

--- js/anm/msg-table.js
+++ js/anm/msg-table.js
@@ -20,60 +20,104 @@ MSG_BY_OPCODE.set('06', {
 });
 
 MSG_BY_OPCODE.set('10', {
-  0: 'end',
-  1: 'th10-show-player',
+  0: 'modern-end',
+  1: 'modern-show-player',
   2: 'th10-show-enemy',
-  3: 'th10-show-textbox',
-  4: 'th10-hide-player',
+  3: 'modern-show-textbox',
+  4: 'modern-hide-player',
   5: 'th10-hide-enemy',
-  6: 'th10-hide-textbox',
-  7: 'th10-focus-player',
+  6: 'modern-hide-textbox',
+  7: 'modern-focus-player',
   8: 'th10-focus-enemy',
-  9: 'th10-skippable',
-  10: 'th10-pause',
-  11: 'th10-enemy-appear',
-  12: 'th10-face-player',
+  9: 'modern-skippable',
+  10: 'modern-pause',
+  11: 'modern-ecl-resume',
+  12: 'modern-face-player',
   13: 'th10-face-enemy',
-  14: 'th10-text-1',
-  15: 'th10-text-2',
-  16: 'th10-text-add-nofuri',
-  17: 'th10-text-clear',
-  18: 'th10-music-boss',
+  14: 'modern-text-1',
+  15: 'modern-text-2',
+  16: 'modern-text-add-nofuri',
+  17: 'modern-text-clear',
+  18: 'modern-music-boss',
   19: 'th10-intro',
-  20: 'th10-stage-bonus',
-  21: 'th10-music-fade',
-  22: 'th10-shake-player',
-  23: 'th10-shake-enemy',
+  20: 'modern-stage-end',
+  21: 'modern-music-fade',
+  22: 'modern-shake-player',
+  23: 'modern-shake-enemy',
 });
 MSG_BY_OPCODE.set('11', {
-  0: 'end',
-  1: 'th10-show-player',
+  0: 'modern-end',
+  1: 'modern-show-player',
   2: 'th10-show-enemy',
-  3: 'th10-show-textbox',
-  4: 'th10-hide-player',
+  3: 'modern-show-textbox',
+  4: 'modern-hide-player',
   5: 'th10-hide-enemy',
-  6: 'th10-hide-textbox',
-  7: 'th10-focus-player',
+  6: 'modern-hide-textbox',
+  7: 'modern-focus-player',
   8: 'th10-focus-enemy',
-  9: 'th10-focus-youkai',
-  10: 'th10-skippable',
-  11: 'th10-pause',
-  12: 'th10-enemy-appear',
-  13: 'th10-face-player',
+  9: 'modern-focus-none',
+  10: 'modern-skippable',
+  11: 'modern-pause',
+  12: 'modern-ecl-resume',
+  13: 'modern-face-player',
   14: 'th10-face-enemy',
-  15: 'th10-text-1',
-  16: 'th10-text-2',
-  17: 'th10-text-add',
-  18: 'th10-text-clear',
-  19: 'th10-music-boss',
+  15: 'modern-text-1',
+  16: 'modern-text-2',
+  17: 'modern-text-add',
+  18: 'modern-text-clear',
+  19: 'modern-music-boss',
   20: 'th10-intro',
-  21: 'th10-stage-bonus',
-  22: 'th10-music-fade',
-  23: 'th10-shake-player',
-  24: 'th10-shake-enemy',
-  25: 'th10-y-offset',
-  26: 'th10-flag-2',
+  21: 'modern-stage-end',
+  22: 'modern-music-fade',
+  23: 'modern-shake-player',
+  24: 'modern-shake-enemy',
+  25: 'modern-y-offset',
+  26: 'modern-flag-2',
 });
+
+MSG_BY_OPCODE.set('12', {
+  ...MSG_BY_OPCODE.get('11'),
+  27: 'modern-music-fade-custom',
+});
+
+MSG_BY_OPCODE.set('128', {
+  ...MSG_BY_OPCODE.get('12'),
+  28: 'modern-callout-pos',
+  29: 'modern-callout-type',
+  30: '128-route-select',
+});
+
+MSG_BY_OPCODE.set('13', {
+  ...MSG_BY_OPCODE.get('128'),
+  31: 'modern-31',
+});
+
+MSG_BY_OPCODE.set('14', {
+  ...MSG_BY_OPCODE.get('13'),
+  2: 'modern-show-enemy',
+  5: 'modern-hide-enemy',
+  8: 'modern-focus-enemy',
+  14: 'modern-face-enemy',
+  20: 'modern-intro',
+  32: 'modern-32',
+});
+
+MSG_BY_OPCODE.set('143', {
+  ...MSG_BY_OPCODE.get('14'),
+  33: 'modern-tutorial',
+});
+
+MSG_BY_OPCODE.set('15', {...MSG_BY_OPCODE.get('14')}); // NOT 143; tutorial is removed
+
+MSG_BY_OPCODE.set('16', {
+  ...MSG_BY_OPCODE.get('15'),
+  33: 'modern-darken-portrait',
+  34: 'modern-highlight-portrait',
+  35: 'modern-lights-out',
+});
+
+MSG_BY_OPCODE.set('165', {...MSG_BY_OPCODE.get('143')}); // VD loses all of TH16's changes; identical to ISC.
+MSG_BY_OPCODE.set('17', {...MSG_BY_OPCODE.get('16')}); // But WBaWC is like HSiFS.
 
 // ==========================================================================
 // ==========================================================================
@@ -84,27 +128,53 @@ export const MSG_INS_DATA = {};
 
 // EoSD
 Object.assign(MSG_INS_DATA, {
-  'end': {
-    sig: '', args: [], desc: `End the script.`
+  'modern-end': {
+    sig: '', args: [], desc: `End the script.`,
   },
-  'th10-show-player': {
-    sig: 'S', args: ['unused'], desc: `Show the player portrait.  The argument is never read.`,
-  },
+  'modern-show-player': {
+    sig: 'S', args: ['silly'], desc: `
+    Show the player portrait.
+
+    [tiphide]
+    * ([game=10]&ndash;[game=th15]) The argument is never read.
+    * ([game=16]&ndash;) If the argument is nonzero, it instead loads a specific (hardcoded) anm script on a specific ANM file in the ECL's \`ANM\` list.  This is used to put Satono on the left in [game=16] Extra.
+    [/tiphide]
+  `},
   'th10-show-enemy': {
-    sig: 'S', args: ['unused'], desc: `Show the enemy portrait.  The argument is never read.`,
+    sig: 'S', args: ['unused'], succ: 'modern-show-enemy', desc: `Show the enemy portrait.  The argument is never read.`,
   },
-  'th10-show-textbox': {sig: '', args: [], desc: `Show the text box.`},
-  'th10-hide-player': {sig: '', args: [], desc: `Hide the player portrait.`},
-  'th10-hide-enemy': {sig: '', args: [], desc: `Hide the enemy portrait. [tiphide](and typically also their intro if still visible)[/tiphide]`},
-  'th10-hide-textbox': {sig: '', args: [], desc: `Hide the text box. (and all text on it)`},
-  'th10-focus-player': {sig: '', args: [], desc: `Indicates that the player is speaking.`},
-  'th10-focus-enemy': {sig: '', args: [], desc: `Indicates that the enemy is speaking.`},
-  'th10-focus-youkai': {sig: '', args: [], wip: 1, desc: `[wip]Probably indicates that the youkai ally from aboveground is speaking.[/wip]`},
-  'th10-skippable': {
+  'modern-show-enemy': {
+    sig: 'S', args: ['who'], desc: `Show the enemy portrait.  The argument is for stages with multiple boss characters.`,
+  },
+  'modern-show-textbox': {sig: '', args: [], desc: `
+    Show the text box.
+
+    [tiphide]
+    In [game=th13] this becomes a nop as callouts are used instead.  (see [ref=msg:th10-callout-type])
+    [/tiphide]
+  `},
+  'modern-hide-player': {sig: '', args: [], desc: `Hide the player portrait.`},
+  'th10-hide-enemy': {
+    sig: '', args: [], succ: 'modern-hide-enemy', desc: `
+    Hide the enemy portrait. [tiphide](and typically also their intro if still visible)[/tiphide]
+  `},
+  'modern-hide-enemy': {sig: 'S', args: ['who'], desc: `Hide an enemy portrait.`},
+  'modern-hide-textbox': {sig: '', args: [], desc: `Hide the text box. (and all text on it)`},
+  'modern-focus-player': {sig: '', args: [], desc: `Indicates that the player is speaking.`},
+  'th10-focus-enemy': {sig: '', args: [], succ: 'modern-focus-enemy', desc: `Indicates that the enemy is speaking.`},
+  'modern-focus-enemy': {sig: 'S', args: ['who'], desc: `Indicates that an enemy is speaking.`},
+  'modern-focus-none': {sig: '', args: [], desc: `
+    Indicates that nobody is speaking.
+
+    [tiphide]
+    Examples: Used in [game=11] for the helper Youkai from aboveground, and in [game=13] for the message at the end of Extra that tells you how to get the secret ending.
+    [/tiphide]
+  `},
+  'modern-skippable': {
     sig: 'S', args: ['skippable'], desc: `
     Sets a boolean bitflag.  When the flag is 1, text can be fast-forwarded.
   `},
-  'th10-pause': {
+  'modern-pause': {
     sig: 'S', args: ['max'], desc: `
     Waits up to some maximum number of frames for the player to press the Shoot key.
 
@@ -113,27 +183,36 @@ Object.assign(MSG_INS_DATA, {
     the time label by any similar amount.
     [/tiphide]
   `},
-  'th10-enemy-appear': {
+  'modern-ecl-resume': {
     sig: '', args: [], desc: `
-    Tells the boss enemy to fly onscreen.
+    Resumes any ECL scripts that are waiting on MSG.  Often used to tell the boss when to fly onscreen.
+
+    [tiphide]
+    Specifically, this resumes ECL scripts that are using the \`dialogWait\` instruction. (instruction 519 in [game=17])
+    [/tiphide]
   `},
-  'th10-face-player': {
-    sig: 'S', args: ['id'], desc: `
+  'modern-face-player': {
+    sig: 'S', args: ['face'], desc: `
     Set the player's facial expression.
     [tiphide]The argument will be added to the ANM script index of the player's first face graphic.[/tiphide]
   `},
   'th10-face-enemy': {
-    sig: 'S', args: ['id'], desc: `
+    sig: 'S', args: ['face'], succ: 'modern-face-enemy', desc: `
     Set the enemy's facial expression.
     [tiphide]The argument will be added to the ANM script index of the enemy's first face graphic.[/tiphide]
   `},
-  'th10-text-1': {sig: 'm', args: ['text'], desc: `Set the first line of text.  Unused.`},
-  'th10-text-2': {sig: 'm', args: ['text'], desc: `Set the second line of text.  Unused.`},
-  'th10-text-add-nofuri': {
-    sig: 'm', args: ['text'], succ: 'th10-text-add', desc: `
+  'modern-face-enemy': {
+    sig: 'S', args: ['who', 'face'], desc: `
+    Set an enemy's facial expression.
+    [tiphide]The argument will be added to the ANM script index of the enemy's first face graphic.[/tiphide]
+  `},
+  'modern-text-1': {sig: 'm', args: ['text'], desc: `Set the first line of text.  Unused.`},
+  'modern-text-2': {sig: 'm', args: ['text'], desc: `Set the second line of text.  Unused.`},
+  'modern-text-add-nofuri': {
+    sig: 'm', args: ['text'], succ: 'modern-text-add', desc: `
     Sets the next line of text.
   `},
-  'th10-text-add': {
+  'modern-text-add': {
     sig: 'm', args: ['text'], desc: `
     Sets the next line of text.
 
@@ -151,40 +230,143 @@ Object.assign(MSG_INS_DATA, {
     [/code]
     [/tiphide]
   `},
-  'th10-text-clear': {sig: '', args: [], desc: `Erases all text. Unused.`},
-  'th10-music-boss': {sig: '', args: [], desc: `
-    Initiates boss music and displays its BGM title text.
-  `},
-  'th10-intro': {sig: '', args: [], desc: `Display the enemy's name and flavor text.`},
-  'th10-stage-bonus': {sig: '', args: [], wip: 1, desc: `
-    Awards the player points for completing a stage.
+  'modern-text-clear': {sig: '', args: [], wip: 1, desc: `
+    Erases all text. (at least, that's what it does pre-[game=128])
 
     [tiphide]
-    [wip]Unlike [ref=msg:th06-stage-next], I don't think this plays any role in actually sending you to the next stage?  Haven't tested.[/wip]
+    [wip]Beginning with [game=128] it also does something to the callout, so I'm not sure how to differentiate it from [ref=msg:th10-hide-textbox].[/wip]
+    [/tiphide]
+
+    [tiphide]
+    Even though this instruction has existed since [game=10], the first game to use it is [game=13].
     [/tiphide]
   `},
-  'th10-music-fade': {sig: '', args: [], desc: `
+  'modern-music-boss': {sig: '', args: [], desc: `
+    Initiates boss music and displays its BGM title text.
+  `},
+  'th10-intro': {sig: '', args: [], succ: 'modern-intro', desc: `
+    Display the enemy's name and flavor text.
+  `},
+  'modern-intro': {sig: 'S', args: ['who'], desc: `
+    Display an enemy's name and flavor text.
+  `},
+  'modern-stage-end': {sig: '', args: [], desc: `
+    Awards the stage bonus and begins the next stage.  (or ends stage practice)
+  `},
+  'modern-music-fade': {sig: '', args: [], desc: `
     Fades music out at the end of the stage.
 
     [tiphide]
     The duration of the fade is hardcoded based on stage number.
     [/tiphide]
   `},
-  'th10-shake-player': {sig: '', args: [], desc: `
-    Make the player portrait shake briefly.
+  'modern-shake-player': {sig: '', args: [], desc: `
+    Make the player portrait shake briefly like Nitori.
   `},
-  'th10-shake-enemy': {sig: '', args: [], desc: `
-    Make the enemy portrait shake briefly.  Nitori uses this when you first see her.
+  'th10-shake-enemy': {sig: '', args: [], succ: 'modern-shake-enemy', desc: `
+    Make the enemy portrait shake briefly (used by Nitori).
   `},
-  'th10-y-offset': {sig: 'S', args: ['dy'], desc: `
+  'modern-shake-enemy': {sig: '', args: [], desc: `
+    Make all of the enemy portraits shake briefly like Nitori.
+  `},
+  'modern-y-offset': {sig: 'S', args: ['dy'], desc: `
     Adds a vertical offset to the position of all text.
 
     [tiphide]
     This sets the y-component of [\`pos_2\`](#anm/concepts&a=position).
     [/tiphide]
   `},
-  'th10-flag-2': {sig: '', args: [], wip: 2, desc: `
+  'modern-flag-2': {sig: '', args: [], wip: 2, desc: `
     Sets an unknown bitflag to 1.
+  `},
+  'modern-music-fade-custom': {
+    sig: 'f', args: ['duration'], desc: `
+    Fades music out over a user-supplied time interval.
+  `},
+  'modern-callout-pos': {
+    sig: 'ff', args: ['x', 'y'], desc: `
+    Sets the position of the callout.
+
+    [tiphide]
+    [wip]What anchor point (presumably center)?  What coordinate system (presumably arcade-region coords)?[/wip]
+    [/tiphide]
+  `},
+  'modern-callout-type': {
+    sig: 'S', args: ['value'], desc: `
+    Sets the callout type.
+
+    [tiphide]
+    The callout type determines the following:
+    * The shape of the callout.  E.g. rounded rectangle (normal), spikey (shouting), thought bubble.
+    * ([game=13]&ndash;) The height of the callout. (1 or 2 lines)
+    * ([game=th17]) The background color. (red for beast speaking)
+    It does not determine the width (this is automatically computed) or the placement of the callout "tip".
+    [/tiphide]
+
+    [tiphide]
+    You can generally find out all available callout types by looking at \`face/balloon.png\` or \`face/balloon_1024.png\` in \`front.anm\`.
+    [/tiphide]
+  `},
+  'th128-route-select': {
+    sig: '', args: [], desc: `
+    Used in the first stage of GFW to let the player pick the second stage.
+
+    [tiphide]
+    In games after GFW, this is a no-op.
+    [/tiphide]
+  `},
+  'modern-31': {
+    sig: 'S', args: ['unused'], wip: 2, desc: `
+    [wip=2]Unknown. Only used in [game=13] stage 4. Argument appears unused.[/wip]
+  `},
+  'modern-32': {
+    sig: 'S', args: ['side'], wip: 2, desc: `
+    [wip=2]Unknown. Only used in [game=14] Extra.[/wip]
+
+    [tiphide]
+    [wip=2]The argument sets the current speaker, similar to [ref=msg:modern-focus-player] and [ref=msg:modern-focus-boss][/wip]
+    [/tiphide]
+  `},
+  'modern-tutorial': {
+    sig: 'S', args: ['id'], desc: `
+    Initiates a tutorial in scene games.
+
+    [tiphide]
+    Note this is a **completely different instruction** from the \`ins_33\` found in [game=16] (which is [ref=msg:modern-darken-portrait]).
+    [/tiphide]
+  `},
+  'modern-darken-portrait': {
+    sig: 'SS', args: ['side', 'who'], desc: `
+    Darkens a single portrait without changing the active speaker.
+
+    [tiphide]
+    If \`side\` is 0, it darkens the player portrait.  If the first argument is nonzero, it darkens the enemy portrait indexed by \`who\`.
+    [/tiphide]
+
+    [tiphide]
+    Note this is a **completely different instruction** from the \`ins_33\` found in [game=143] and [game=165] (which is [ref=msg:modern-tutorial]),
+    and on top of that it is entirely unused!
+    [/tiphide]
+  `},
+  'modern-highlight-portrait': {
+    sig: 'SS', args: ['side', 'who'], desc: `
+    Highlight a single portrait as if they are speaking (but without changing the active speaker).
+
+    [tiphide]
+    If \`side\` is 0, it highlights the player portrait.  If the first argument is nonzero, it highlights the enemy portrait indexed by \`who\`.
+    [/tiphide]
+
+    [tiphide]
+    This is used in [game=16] Cirno Stage 1 to make Cirno and Eternity shout in unison at each other.
+    [/tiphide]
+  `},
+  'modern-lights-out': {
+    sig: '', args: [], desc: `
+    Covers the stage background and enemies in black.
+
+    [tiphide]
+    Used at the end of [game=16] Cirno stage 4.
+    [/tiphide]
   `},
 });
 
