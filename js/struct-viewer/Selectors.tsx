7797c81924b6bdaddc789c6ac3fc77b49d07b24b
Michael Lamparski <diagonaldevice@gmail.com>
2021-07-13 01:49:32 -0400

    use material-ui Autocomplete for selectors

--- js/struct-viewer/Selectors.tsx
+++ js/struct-viewer/Selectors.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import clsx from 'clsx';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import TextField from '@material-ui/core/TextField';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import {StructDatabase, TypeName, Version, VersionLevel} from './database';
 
 export function Selectors(props: {
   db: StructDatabase,
-  setStruct: React.Dispatch<TypeName>,
-  setVersion: React.Dispatch<Version>,
+  setStruct: React.Dispatch<TypeName | null>,
+  setVersion: React.Dispatch<Version | null>,
   minLevel: VersionLevel,
   struct: TypeName | null,
   version: Version | null,
@@ -17,18 +25,28 @@ export function Selectors(props: {
     setShowUnavailable(ev.target.checked)
   }, [setShowUnavailable]);
 
-  return <>
-    <div><label>
-      <input type="checkbox" checked={showUnavailable} onChange={handleUnavailableCheck}/>
-      Show unavailable
-    </label></div>
+  // FIXME: Error for incompatible struct/version
+  const error = struct === null || version === null;
+
+  return <FormControl component="fieldset" error={error}>
+    <FormLabel component="legend">Navigation</FormLabel>
+    <FormControlLabel
+      control={<Checkbox
+        color="primary"
+        checked={showUnavailable}
+        onChange={handleUnavailableCheck}
+      />}
+      label="Show unavailable"
+      labelPlacement="end"
+    />
     <StructPicker onChange={setStruct} {...{db, showUnavailable, struct, version}}/>
     <VersionPicker onChange={setVersion} {...{db, showUnavailable, struct, version, minLevel}}/>
-  </>;
+    {error && <FormHelperText>Select a struct and version</FormHelperText>}
+  </FormControl>;
 }
 
 export function StructPicker(props: {
-  onChange: React.Dispatch<TypeName>,
+  onChange: React.Dispatch<TypeName | null>,
   db: StructDatabase,
   showUnavailable: boolean,
   struct: TypeName | null,
@@ -39,11 +57,14 @@ export function StructPicker(props: {
     getStructOptions({db, showUnavailable, version})
   ), [db, showUnavailable, version]);
 
-  return <AsyncSelector onChange={onChange} optionsPromise={optionsPromise} current={struct}/>
+  return <AsyncSelector
+    label="Struct" onChange={onChange} optionsPromise={optionsPromise}
+    current={struct}
+  />;
 }
 
 export function VersionPicker(props: {
-  onChange: React.Dispatch<Version>,
+  onChange: React.Dispatch<Version | null>,
   db: StructDatabase,
   showUnavailable: boolean,
   struct: TypeName | null,
@@ -55,30 +76,39 @@ export function VersionPicker(props: {
     getVersionOptions({db, showUnavailable, struct, minLevel})
   ), [db, showUnavailable, struct, minLevel]);
 
-  return <AsyncSelector onChange={onChange} optionsPromise={optionsPromise} current={version}/>
+  return <AsyncSelector
+    label="Version" onChange={onChange} optionsPromise={optionsPromise}
+    current={version}
+  />;
 }
 
 type Option<T> = {value: T, available: boolean};
 type SelectorProps<T> = {
-  onChange: React.Dispatch<T>,
+  onChange: React.Dispatch<T | null>,
   options: Option<T>[],
   current: T | null,
   placeholderValue?: string,
+  label: string,
+  loading: boolean,
 };
 
-function AsyncSelector<T extends string>(
-  props: Omit<SelectorProps<T>, "options"> & {
-    optionsPromise: Promise<Option<T>[]>,
-  },
-) {
+type AsyncSelectorProps<T> = Omit<SelectorProps<T>, "options" | "loading"> & {
+  optionsPromise: Promise<Option<T>[]>,
+};
+
+function AsyncSelector<T extends string>(props: AsyncSelectorProps<T>) {
   const {optionsPromise} = props;
   // Begin with an empty option list until the promise resolves.
   const [options, setOptions] = React.useState<Option<T>[]>([]);
+  // FIXME: loading indicator never disappears after clearing one of the selectors.
+  const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
     const abortController = new AbortController();
+    setLoading(true);
     optionsPromise.then((options) => {
       if (!abortController.signal.aborted) {
+        setLoading(false);
         setOptions(options);
       }
     });
@@ -86,23 +116,55 @@ function AsyncSelector<T extends string>(
     return () => abortController.abort();
   }, [optionsPromise])
 
-  return <Selector {...props} options={options} />
+  return <Selector {...props} {...{options, loading}} />
 }
 
 function Selector<T extends string>(props: SelectorProps<T>) {
-  const {onChange, options, current, placeholderValue = ''} = props;
-  const handleChange = React.useCallback((ev: React.ChangeEvent<HTMLSelectElement>) => {
-    onChange(ev.target.value as T);
-  }, [onChange]);
+  const {onChange, options, current, loading, label} = props;
+  const currentOption = useOptionFromLabel(options, ({value}) => value, current);
 
-  return <select value={current || placeholderValue} onChange={handleChange}>
-    <option key={placeholderValue} value={placeholderValue}></option>
-    {options.map((item) => (
-      <option key={item.value} value={item.value} className={clsx({'unavailable': !item.available})}>
-        {item.value}
-      </option>
-    ))}
-  </select>
+  return (
+    <Autocomplete
+      style={{ width: 300 }}
+      value={currentOption}
+      getOptionSelected={(s: Option<T>) => s.value === current}
+      onChange={(_, option) => onChange(option && option.value as T)}
+      options={options}
+      renderOption={({available, value}) => (
+        <span className={clsx({'unavailable': !available})}>{value}</span>
+      )}
+      getOptionLabel={({value}) => value}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={label}
+          size="small"
+          variant="outlined"
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: <>
+                {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                {params.InputProps.endAdornment}
+            </>,
+          }} // InputProps
+        ></TextField>
+      )} // renderInput
+    ></Autocomplete>
+  );
+}
+
+/** Recover the object for the current option that corresponds to a given label. */
+function useOptionFromLabel<T>(options: T[], getLabel: (option: T) => string, label: string | null) {
+  const optionLookupFromLabel = React.useMemo(() => {
+    return new Map([...options.map((option) => [getLabel(option), option] as const)])
+  }, [options])
+
+  const optionForValue = React.useMemo(() => {
+    if (!label) return null;
+    return optionLookupFromLabel.get(label);
+  }, [label, optionLookupFromLabel]);
+
+  return optionForValue;
 }
 
 async function getStructOptions(args: {
